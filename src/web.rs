313004c5e6a17790a2e9d63bcba48909ef5e1023
takeshiemoto <private.takeshiemoto@gmail.com>
2023-11-29 23:11:44 +0900

    dev

--- src/web.rs
+++ src/web.rs
@@ -1,3 +1,22 @@
-pub fn run() {
-    println!("Hello from web.rs");
+use axum::routing::get;
+use std::net::SocketAddr;
+
+#[tokio::main]
+pub async fn run() {
+    let app = axum::Router::new().route(
+        "/",
+        get(|| async {
+            {
+                "Hello world!"
+            }
+        }),
+    );
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
